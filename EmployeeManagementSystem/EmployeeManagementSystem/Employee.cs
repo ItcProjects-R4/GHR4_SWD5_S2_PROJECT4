da5c5a4ad867f6578c064ac43acfb9dfb7054655
shahd muhammed ali gaballah <shahdgabllah@gmail.com>
2026-03-04 18:23:00 +0200

    initiate project structure

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public JobType JobType { get; set; }
+        public bool IsActive { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nName: {Name}\nSalary: {Salary}\nJob type: {JobType}\nIs active: {IsActive}";
+        }
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/EmployeeManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    public class EmployeeManager
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public void AddEmployee(Employee emp)
+        {
+           
+        }
+
+        public void ViewAllEmployees()
+        {
+            
+        }
+
+        public void SearchEmployee(int empId)
+        {
+           
+        }
+        
+        public void FilterEmployees()
+        {
+           
+        }
+    }
+}
