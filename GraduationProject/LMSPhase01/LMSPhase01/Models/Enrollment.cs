7952906dbb585d78bebfb21742f50ff73e36cad2
Moataz Gado <138323638+Moatazgado@users.noreply.github.com>
2026-04-09 19:31:23 +0200

    Create Enrollment model with navigation properties
    Added properties for User, Course, and Payment navigation.

--- GraduationProject/LMSPhase01/LMSPhase01/Models/Enrollment.cs
+++ GraduationProject/LMSPhase01/LMSPhase01/Models/Enrollment.cs
@@ -5,6 +5,21 @@ using System.Text;
 namespace LMSPhase01.Models
 {
     public class Enrollment
-    {
+    { 
+        
+    public int Id { get; set; }    // Primary Key
+
+    // Foreign Keys
+    public int UserId { get; set; }
+    public int CourseId { get; set; }
+
+    public DateTime EnrolledAt { get; set; }
+    public string Status { get; set; }
+
+    // Navigation Properties
+    public ApplicationUser User { get; set; }
+    public Course Course { get; set; }
+    public Payment Payment { get; set; }
+
     }
 }
