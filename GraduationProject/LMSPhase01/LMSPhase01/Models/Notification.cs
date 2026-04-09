3aad21d920d8ffb12cca4b424968b90fd28884d0
Amira Khamis <amirakhamis885@gmail.com>
2026-04-09 14:48:52 +0200

    completed Notifications and SubmissionFiles

--- GraduationProject/LMSPhase01/LMSPhase01/Models/Notification.cs
+++ GraduationProject/LMSPhase01/LMSPhase01/Models/Notification.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+
 
 namespace LMSPhase01.Models
 {
     public class Notification
     {
+        public int Id { get; set; }  
+
+        [Required]
+        public string UserId { get; set; }  
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        public bool IsRead { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Navigation
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
     }
 }
