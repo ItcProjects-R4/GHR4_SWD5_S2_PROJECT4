6046df602ebf294ae66cbc4aa80a41efea2840bf
Moataz Gado <138323638+Moatazgado@users.noreply.github.com>
2026-04-09 19:33:26 +0200

    Update Payment.cs

--- GraduationProject/LMSPhase01/LMSPhase01/Models/Payment.cs
+++ GraduationProject/LMSPhase01/LMSPhase01/Models/Payment.cs
@@ -5,6 +5,18 @@ using System.Text;
 namespace LMSPhase01.Models
 {
     public class Payment
-    {
-    }
+{
+    public int Id { get; set; }        // Primary Key
+        
+    // Foreign Key
+    public int EnrollmentId { get; set; }
+
+    public decimal Amount { get; set; }
+    public string Status { get; set; }
+    public string PaymentMethod { get; set; }
+    public string TransactionId { get; set; }
+    public DateTime PaidAt { get; set; }
+
+    public Enrollment Enrollment { get; set; }
+}
 }
