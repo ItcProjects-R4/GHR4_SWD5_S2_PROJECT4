698d51fed000264867817756fb2eb52c0c4504e9
Amira Khamis <amirakhamis885@gmail.com>
2026-04-09 19:06:33 +0200

    submissionfiles

--- GraduationProject/LMSPhase01/LMSPhase01/Models/SubmissionFile.cs
+++ GraduationProject/LMSPhase01/LMSPhase01/Models/SubmissionFile.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+
+
 
 namespace LMSPhase01.Models
 {
-    public class SubmissionFiles
+    public class SubmissionFile
     {
+        public int Id { get; set; }  
+
+        [Required]
+        public int SubmissionId { get; set; }  
+
+        [Required]
+        public string FileUrl { get; set; }
+
+        public string? FileName { get; set; }
+
+        public string? FileType { get; set; }
+
+        public double? FileSize { get; set; }
+
+        // Navigation
+        [ForeignKey("SubmissionId")]
+        public Submission Submission { get; set; }
     }
 }
