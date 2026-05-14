d549559875aa3e796c5eab79c53c004cd5384f23
shahdgaballah <shahdgabllah@gmail.com>
2026-05-14 20:52:53 +0300

    implement CloudinaryService for media upload

--- LMS/LMS.BLL/Services/Implementation/CloudinaryService.cs
+++ LMS/LMS.BLL/Services/Implementation/CloudinaryService.cs
@@ -1,4 +1,6 @@
-﻿using LMS.BLL.Services.Interfaces;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using LMS.BLL.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
@@ -8,19 +10,43 @@ namespace LMS.BLL.Services.Implementation
 {
     public class CloudinaryService : ICloudinaryService
     {
-        public Task<string> UploadFileAsync(IFormFile file)
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService(Cloudinary cloudinary)
         {
-            throw new NotImplementedException();
+            _cloudinary = cloudinary;
+        }
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            using var stream = file.OpenReadStream();
+            var result = await _cloudinary.UploadAsync(new RawUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = "lms/files"
+            });
+            return result.SecureUrl.ToString();
         }
 
-        public Task<string> UploadImageAsync(IFormFile file)
+        public async Task<string> UploadImageAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            using var stream = file.OpenReadStream();
+            var result = await _cloudinary.UploadAsync(new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = "lms/images"
+            });
+            return result.SecureUrl.ToString();
         }
 
-        public Task<string> UploadVideoAsync(IFormFile file)
+        public async Task<string> UploadVideoAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            using var stream = file.OpenReadStream();
+            var result = await _cloudinary.UploadAsync(new VideoUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = "lms/videos"
+            });
+            return result.SecureUrl.ToString();
         }
     }
 }
