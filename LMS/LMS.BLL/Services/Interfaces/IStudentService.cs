87e36865a1b1aee2e5e821ecbbc737b813f53bd2
hodaa <hodaaahmed138@gmail.com>
2026-06-06 15:31:32 +0300

    Add loading and instructor/user

---
+++ LMS/LMS.BLL/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<ApplicationUser>> GetFilteredUsersAsync(string searchString, string roleFilter);
+    }
+}
