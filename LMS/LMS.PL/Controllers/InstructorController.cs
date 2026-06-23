bd0f3b4d0fcfc128119e8c0fecb1ec96353cfbed
shahdgaballah <shahdgabllah@gmail.com>
2026-06-23 15:16:57 +0300

    fixing braces

--- LMS/LMS.PL/Controllers/InstructorController.cs
+++ LMS/LMS.PL/Controllers/InstructorController.cs
@@ -174,12 +174,15 @@ namespace LMS.PL.Controllers
                 return PartialView("_AssignmentDetails", assignment);
 
             return View(assignment);
+
+        }
+            
         [Authorize(Roles = "Instructor")]
         public async Task<IActionResult> Settings()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null) return NotFound();
-
+           
             var viewModel = new InstructorSettingsViewModel
             {
                 FirstName = user.FirstName,
@@ -188,7 +191,7 @@ namespace LMS.PL.Controllers
                 PhoneNumber = user.PhoneNumber ?? string.Empty
             };
 
-            return View(viewModel);
+                return View(viewModel);
         }
 
         // Istructor profile settings
@@ -201,28 +204,28 @@ namespace LMS.PL.Controllers
             {
                 return View(model);
             }
-
+       
             var user = await _userManager.GetUserAsync(User);
             if (user == null) return NotFound();
-
+       
             user.FirstName = model.FirstName;
             user.LastName = model.LastName;
             user.Email = model.Email;
             user.PhoneNumber = model.PhoneNumber;
-
+       
             var result = await _userManager.UpdateAsync(user);
             if (result.Succeeded)
             {
                 TempData["SuccessMessage"] = "Profile settings updated successfully.";
                 return RedirectToAction(nameof(Settings));
             }
-
+       
             foreach (var error in result.Errors)
             {
                 ModelState.AddModelError(string.Empty, error.Description);
             }
-
+       
             return View(model);
+            }
         }
-    }
 }
