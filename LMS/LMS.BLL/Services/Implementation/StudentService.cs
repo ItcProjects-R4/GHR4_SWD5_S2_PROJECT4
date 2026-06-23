98f238a8fa52283de19f2336c3bdd6cb23a8ea7c
shahdgaballah <shahdgabllah@gmail.com>
2026-06-24 02:39:01 +0300

    generic profile update system and implement student settings page

--- LMS/LMS.BLL/Services/Implementation/StudentService.cs
+++ LMS/LMS.BLL/Services/Implementation/StudentService.cs
@@ -11,10 +11,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LMS.BLL.Services.Implementation
 {
-    public class
-       StudentService(IApplicationDbContext context, ICurrentUserService currentUserService,
-       UserManager<ApplicationUser> userManager, ICloudinaryService cloudinaryService,
-       ICheckoutService checkoutService)
+    public class StudentService(
+        IApplicationDbContext context,
+        ICurrentUserService currentUserService,
+       UserManager<ApplicationUser> userManager, 
+       ICloudinaryService cloudinaryService,
+       ICheckoutService checkoutService
+       )
        : IStudentService
     {
         public async Task<IEnumerable<ApplicationUser>> GetFilteredUsersAsync(string searchString, string roleFilter)
@@ -517,5 +520,6 @@ namespace LMS.BLL.Services.Implementation
                 .SelectMany(m => m.Contents)
                 .FirstOrDefault()?.Id;
         }
+
     }
 }
---
+++ LMS/LMS.Domain/ViewModels/UpdatePasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LMS.Domain.ViewModels
+{
+    public class UpdatePasswordViewModel
+    {
+        [Required(ErrorMessage = "Current password is required.")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        [Required(ErrorMessage = "New password is required.")]
+        [StringLength(100, ErrorMessage = "Password must be at least {2} characters.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
